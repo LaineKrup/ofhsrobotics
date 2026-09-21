49c37ed88cc61362b302bffa51310d233912f64a
LaineKrup <lainekrup@gmail.com>
2026-09-21 19:05:47 -0400

    Configure settings for production deployment
    Updated settings for production environment by using environment variables for SECRET_KEY, disabling DEBUG, and setting ALLOWED_HOSTS and STATIC_ROOT.

--- ofhsrobotics/settings.py
+++ ofhsrobotics/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-np1u6$%5xb+43z7mh++8g0h#zwn3ank(lsz%keidz7)ywqybht'
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["ofhsrobotics.pythonanywhere.com"]
 
 
 # Application definition
@@ -115,7 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_FROM_EMAIL = 'noreply@ofhsrobotics.local'
 
